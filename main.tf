5edb043c6d322ce9cd8215e92b73dafe973dc16b
Andrew Hughes <andrewhughes1988@gmail.com>
2025-09-27 16:42:10 -0500

    added first subnet to vnetTF

--- main.tf
+++ main.tf
@@ -26,4 +26,14 @@ resource "azurerm_virtual_network" "vnetTF" {
   address_space       = ["10.0.0.0/8"]
   location            = "eastus"
   resource_group_name = azurerm_resource_group.rg.name
+  tags = {
+    Department = "Terraform"
+  }
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnetTF.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
