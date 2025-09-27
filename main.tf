8f2ac56e7f9bc86c04df33b3b3cac4ab29d451de
Andrew Hughes <andrewhughes1988@gmail.com>
2025-09-27 16:35:21 -0500

    added vnet to rg

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~> 3.0.2"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
     }
   }
   required_version = ">= 1.1.0"
@@ -10,11 +10,20 @@ terraform {
 
 provider "azurerm" {
   features {
-    
+
   }
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = "terraform_RG"
-    location = "westus"  
+  name     = "terraform_RG"
+  location = "eastus"
+  tags = {
+  Department = "Terraform" }
+}
+
+resource "azurerm_virtual_network" "vnetTF" {
+  name                = "vnetTF"
+  address_space       = ["10.0.0.0/8"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg.name
 }
